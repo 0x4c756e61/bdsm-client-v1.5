f69df3023e3da562f64ba09f2b813ff0fc52adba
Firmin B <stereogaming18@gmail.com>
2023-03-24 12:55:41 +0100

    🛠️ Code improvements

--- src/render.js
+++ src/render.js
@@ -164,9 +164,8 @@ electron.ipcRenderer.invoke("getDataPath").then(async (dataPath) => {
         },
       });
 
-      let outlineColor = "#2eff8c";
       const data = JSON.parse(response.data);
-      div.style.setProperty("--outline-color", outlineColor);
+      div.style.setProperty("--outline-color", "#2eff8c");
       div.innerHTML = `<div onclick="window.editServer(${index})" class="card-btn card-edit">
       <svg aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.2929 9.8299L19.9409 9.18278C21.353 7.77064 21.353 5.47197 19.9409 4.05892C18.5287 2.64678 16.2292 2.64678 14.817 4.05892L14.1699 4.70694L19.2929 9.8299ZM12.8962 5.97688L5.18469 13.6906L10.3085 18.813L18.0201 11.0992L12.8962 5.97688ZM4.11851 20.9704L8.75906 19.8112L4.18692 15.239L3.02678 19.8796C2.95028 20.1856 3.04028 20.5105 3.26349 20.7337C3.48669 20.9569 3.8116 21.046 4.11851 20.9704Z" fill="currentColor"></path></svg>
     </div><div onclick="window.deleteServer(${index});" class="card-btn card-delete">
