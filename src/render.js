8058963823dcf4546aed283f3e925f242c1ebf9a
Firmin_ <40271484+firminsurgithub@users.noreply.github.com>
2023-05-04 22:14:34 +0200

    🛠️ Code improvements

--- src/render.js
+++ src/render.js
@@ -203,16 +203,27 @@ electron.ipcRenderer.invoke("getDataPath").then(async (dataPath) => {
 
   /* At each data change, update the selected server in the list */
   async function updateServer(index, server) {
+    /* Create the div before the fetch to avoid disordered data in the list */
+    let div = document.querySelector(`#server-${index}`);
+    if (!div) {
+      document.querySelector(
+        "#gridLayout"
+      ).innerHTML += `<div class="card" id="server-${index}"><div onclick="window.editServer(${index})" class="card-btn card-edit">
+        <svg aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.2929 9.8299L19.9409 9.18278C21.353 7.77064 21.353 5.47197 19.9409 4.05892C18.5287 2.64678 16.2292 2.64678 14.817 4.05892L14.1699 4.70694L19.2929 9.8299ZM12.8962 5.97688L5.18469 13.6906L10.3085 18.813L18.0201 11.0992L12.8962 5.97688ZM4.11851 20.9704L8.75906 19.8112L4.18692 15.239L3.02678 19.8796C2.95028 20.1856 3.04028 20.5105 3.26349 20.7337C3.48669 20.9569 3.8116 21.046 4.11851 20.9704Z" fill="currentColor"></path></svg>
+        </div><div onclick="window.deleteServer(${index});" class="card-btn card-delete">
+        <svg aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M15 3.999V2H9V3.999H3V5.999H21V3.999H15Z"></path><path fill="currentColor" d="M5 6.99902V18.999C5 20.101 5.897 20.999 7 20.999H17C18.103 20.999 19 20.101 19 18.999V6.99902H5ZM11 17H9V11H11V17ZM15 17H13V11H15V17Z"></path></svg>
+    </div>
+    <div onclick="window.viewServer(${index})" class="card-btn card-view">
+      <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
+      </div>
+      <div class="card-title" id="card-title-${index}">${server.prettyname.toLowerCase()} <span id="card-id">(---)</span></div>
+      <div class="card-platform" id="card-platform-${index}">Running: ---</div>
+      <div class="card-usage" id="card-usage-${index}">RAM: ---</div>
+      <div cladd="card-cpu-usage" id="card-cpu-usage-${index}">CPU: ---%</div>
+      <div class="card-status" id="card-status-${index}">❓</div></div>`;
+    }
     /* Try/Catch to detect if server is not responding */
     try {
-      /* Create the div before the fetch to avoid disordered data in the list */
-      let div = document.querySelector(`#server-${index}`);
-      if (!div) {
-        document.querySelector("#gridLayout").innerHTML += `
-        <div class="card" id="server-${index}"></div>
-        `;
-      }
-
       const response = await axios({
         method: "post",
         url: `http://${server.ip}:${server.port}/update`,
@@ -224,32 +235,27 @@ electron.ipcRenderer.invoke("getDataPath").then(async (dataPath) => {
 
       const data = JSON.parse(response.data);
       div.style.setProperty("--outline-color", "#2eff8c");
-      div.innerHTML = `<div onclick="window.editServer(${index})" class="card-btn card-edit">
-      <svg aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.2929 9.8299L19.9409 9.18278C21.353 7.77064 21.353 5.47197 19.9409 4.05892C18.5287 2.64678 16.2292 2.64678 14.817 4.05892L14.1699 4.70694L19.2929 9.8299ZM12.8962 5.97688L5.18469 13.6906L10.3085 18.813L18.0201 11.0992L12.8962 5.97688ZM4.11851 20.9704L8.75906 19.8112L4.18692 15.239L3.02678 19.8796C2.95028 20.1856 3.04028 20.5105 3.26349 20.7337C3.48669 20.9569 3.8116 21.046 4.11851 20.9704Z" fill="currentColor"></path></svg>
-    </div><div onclick="window.deleteServer(${index});" class="card-btn card-delete">
-      <svg aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M15 3.999V2H9V3.999H3V5.999H21V3.999H15Z"></path><path fill="currentColor" d="M5 6.99902V18.999C5 20.101 5.897 20.999 7 20.999H17C18.103 20.999 19 20.101 19 18.999V6.99902H5ZM11 17H9V11H11V17ZM15 17H13V11H15V17Z"></path></svg>
-    </div>
-    <div onclick="window.viewServer(${index})" class="card-btn card-view">
-      <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
-    </div>
-      <div class="card-title">${server.prettyname.toLowerCase()} <span id="card-id">(${
+      document.querySelector(
+        `#card-title-${index}`
+      ).innerHTML = `${server.prettyname.toLowerCase()} <span id="card-id">(${
         data.serverId
-      })</span></div>
-      <div class="card-platform">Running: ${data.osPlatform}</div>
-      <div class="card-usage">RAM: ${data.ramUsage} (${data.ramPercent})</div>
-      <div class="card-cpu-usage">CPU: ${data.cpuUsage.toFixed(2)} %</div>
-      <div class="card-status">🟢</div>`;
+      })</span>`;
+      document.querySelector(
+        `#card-platform-${index}`
+      ).innerHTML = `Running: ${data.osPlatform}`;
+      document.querySelector(
+        `#card-usage-${index}`
+      ).innerHTML = `RAM: ${data.ramUsage} (${data.ramPercent})`;
+      document.querySelector(
+        `#card-cpu-usage-${index}`
+      ).innerHTML = `CPU: ${data.cpuUsage.toFixed(2)}%`;
+      console.log(data);
+      document.querySelector(`#card-status-${index}`).innerHTML = `🟢`;
 
       if (viewingIndex == index) {
         updateViewServer(data, server, false);
       }
     } catch (error) {
-      let div = document.querySelector(`#server-${index}`);
-      if (!div) {
-        document.querySelector(
-          "#gridLayout"
-        ).innerHTML += `<div id="server-${index}" class="card"></div>`;
-      }
       warnStatus = true;
       let status = "🔴";
       let outlineColor = "#ff4943";
@@ -270,19 +276,18 @@ electron.ipcRenderer.invoke("getDataPath").then(async (dataPath) => {
         }
       }
       div.style.setProperty("--outline-color", outlineColor);
-      div.innerHTML = `<div onclick="window.editServer(${index})" class="card-btn card-edit">
-      <svg aria-hidden="true" role="img" width="16" height="16" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M19.2929 9.8299L19.9409 9.18278C21.353 7.77064 21.353 5.47197 19.9409 4.05892C18.5287 2.64678 16.2292 2.64678 14.817 4.05892L14.1699 4.70694L19.2929 9.8299ZM12.8962 5.97688L5.18469 13.6906L10.3085 18.813L18.0201 11.0992L12.8962 5.97688ZM4.11851 20.9704L8.75906 19.8112L4.18692 15.239L3.02678 19.8796C2.95028 20.1856 3.04028 20.5105 3.26349 20.7337C3.48669 20.9569 3.8116 21.046 4.11851 20.9704Z" fill="currentColor"></path></svg>
-    </div><div onclick="window.deleteServer(${index});" class="card-btn card-delete">
-      <svg aria-hidden="true" role="img" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M15 3.999V2H9V3.999H3V5.999H21V3.999H15Z"></path><path fill="currentColor" d="M5 6.99902V18.999C5 20.101 5.897 20.999 7 20.999H17C18.103 20.999 19 20.101 19 18.999V6.99902H5ZM11 17H9V11H11V17ZM15 17H13V11H15V17Z"></path></svg>
-    </div>
-    <div onclick="window.viewServer(${index})" class="card-btn card-view">
-      <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
-    </div>
-      <div class="card-title">${server.prettyname.toLowerCase()} <span id="card-id">(-----)</span></div>
-      <div class="card-platform">Running: -----</div>
-      <div class="card-usage">RAM: ----- (----- %)</div>
-      <div class="card-cpu-usage">CPU: ----- %</div>
-      <div class="card-status">${status}</div>`;
+      document.querySelector(
+        `#card-title-${index}`
+      ).innerHTML = `${server.prettyname.toLowerCase()} <span id="card-id">(---)</span>`;
+      document.querySelector(
+        `#card-platform-${index}`
+      ).innerHTML = `Running: ---}`;
+      document.querySelector(`#card-usage-${index}`).innerHTML = `RAM: ---`;
+      document.querySelector(
+        `#card-cpu-usage-${index}`
+      ).innerHTML = `CPU: ---%`;
+      console.log(data);
+      document.querySelector(`#card-status-${index}`).innerHTML = status;
       if (viewingIndex == index) {
         updateViewServer(null, server, true);
       }
