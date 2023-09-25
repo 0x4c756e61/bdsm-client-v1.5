92c3d5d8beaec7d247de78d0f6bd46dbca202cde
Zarox28 <kujawskialexian@gmail.com>
2023-09-25 09:19:10 +0200

    Copy IP

--- src/render.js
+++ src/render.js
@@ -286,8 +286,24 @@ const dragArea = document.querySelector(".drag-area");
         viewPlatform.innerHTML = error ? "---" : data.osType;
         viewOS.innerHTML = error ? "---" : data.osVersion;
         viewCpuModel.innerHTML = error ? "---" : `${data.cpuList[0].model} <span class="detail">(${data.cpuList.length > 1 ? data.cpuList.length + " cores" : "1 core"})</span>`;
-        viewServerIP.innerHTML = settingsList.confidentialMode ? "HIDDEN" : `${server.ip}`;
+
+        if (!settingsList.confidentialMode) {
+            const viewServerIPText = viewServerIP.innerHTML = `${server.ip} <svg id="ip-clipboard-logo" xmlns="http://www.w3.org/2000/svg" height="0.7em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#aaaaaac2}</style><path d="M280 64h40c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128C0 92.7 28.7 64 64 64h40 9.6C121 27.5 153.3 0 192 0s71 27.5 78.4 64H280zM64 112c-8.8 0-16 7.2-16 16V448c0 8.8 7.2 16 16 16H320c8.8 0 16-7.2 16-16V128c0-8.8-7.2-16-16-16H304v24c0 13.3-10.7 24-24 24H192 104c-13.3 0-24-10.7-24-24V112H64zm128-8a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/></svg>`;
+
+            document.querySelector("#ip-clipboard-logo").style.setProperty("cursor", "pointer");
+
+            document.querySelector("#ip-clipboard-logo").addEventListener("click", () => {
+                electron.clipboard.writeText(server.ip);
+
+                viewServerIP.innerHTML = `${server.ip} <svg xmlns="http://www.w3.org/2000/svg" height="0.7em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#2eff8c}</style><path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM305 273L177 401c-9.4 9.4-24.6 9.4-33.9 0L79 337c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L271 239c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>`;
+            
+                setTimeout(() => { viewServerIPText }, 1500);
+            });
         
+        } else {
+            viewServerIP.innerHTML = "HIDDEN";
+        }
+
         if (settingsList.discordRichPresence) {
             electron.ipcRenderer.send('update-rpc', { details: `Viewing ${server.prettyname}`, state: error ? "Server offline" : `CPU : ${data.cpuUsage.toFixed(2)}% | RAM : ${data.ramPercent}` });
         }
